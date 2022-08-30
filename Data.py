6a68a00113259a28470562104f2f9362bbf820e8
owlbase <112480299+owlbase@users.noreply.github.com>
2022-08-30 21:34:33 +0700

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,58 @@
+# Credits: @mrismanaziz
+# FROM File-Sharing-Man <https://github.com/owlbase/filesharing>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram.types import InlineKeyboardButton
+
+class Data:
+    HELP = """
+<b> ❏ Perintah untuk Pengguna BOT
+ ├ /start - Mulai Bot
+ ├ /about - Tentang Bot ini
+ ├ /help - Bantuan Perintah Bot ini
+ ├ /ping - Untuk mengecek bot hidup
+ └ /uptime - Untuk melihat status bot 
+ 
+ ❏ Perintah Untuk Admin BOT
+ ├ /logs - Untuk melihat logs bot
+ ├ /vars - Untuk melihat variable bot
+ ├ /setvar - Untuk mengatur var dengan command dibot
+ ├ /delvar - Untuk menghapus var dengan command dibot
+ ├ /getvar - Untuk melihat salah satu var dengan command dibot
+ ├ /users - Untuk melihat statistik pengguna bot
+ ├ /batch - Untuk membuat link lebih dari satu file
+ ├ /speedtest - Untuk Mengetes kecepatan server bot
+ └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
+
+👨‍💻 Develoved by @OwlBase_Id</b>
+"""
+
+    close = [
+        [InlineKeyboardButton("Tutup", callback_data="close")]
+    ]
+
+    mbuttons = [
+        [
+            InlineKeyboardButton("Help & Commands", callback_data="help"),
+            InlineKeyboardButton("Tutup", callback_data="close")
+        ],
+    ]
+
+    buttons = [
+        [
+            InlineKeyboardButton("Tentang saya", callback_data="about"),
+            InlineKeyboardButton("Tutup", callback_data="close")
+        ],
+    ]
+
+    ABOUT = """
+<b>Tentang Bot ini:
+
+@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+
+ • Creator: @{}
+ • Source Code: <a href='https://github.com/owlbase/filesharing'>filesharing</a>
+ • Owner Repo: @itsme_owl
+
+👨‍💻 Develoved by @itsme_owl</b>
+"""
