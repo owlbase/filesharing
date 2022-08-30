a1e7a7014e6557e9d814b32521e1ee8be508fdd7
owlbase <112480299+owlbase@users.noreply.github.com>
2022-08-30 21:00:22 +0700

    Create button.py

---
+++ plugins/button.py
@@ -0,0 +1,109 @@
+# Credits: @mrismanaziz
+# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from config import FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
+from pyrogram.types import InlineKeyboardButton
+
+
+def start_button(client):
+    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Help & Commands", callback_data="help"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
+            ],
+        ]
+        return buttons
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Grup", url=client.invitelink2),
+            ],
+            [
+                InlineKeyboardButton(text="Help & Commands", callback_data="help"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
+            ],
+        ]
+        return buttons
+    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Channel", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="Help & Commamds", callback_data="help"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
+            ],
+        ]
+        return buttons
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Help & Commands", callback_data="help"),
+            ],
+            [
+                InlineKeyboardButton(text="Channel", url=client.invitelink),
+                InlineKeyboardButton(text="Group", url=client.invitelink2),
+            ],
+            [InlineKeyboardButton(text="Tutup", callback_data="close")],
+        ]
+        return buttons
+
+
+def fsub_button(client, message):
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Grup", url=client.invitelink2),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel", url=client.invitelink),
+                InlineKeyboardButton(text="Join Grup", url=client.invitelink2),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba Lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
